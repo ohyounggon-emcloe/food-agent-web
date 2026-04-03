3606d526f0df8ba6aed0a297a003c9264e3b2bf7
ohyounggon-emcloe <mst_con@emcloe.com>
2026-04-03 19:22:52 +0900

    fix: 지도 높이 320px → 450px 확대

--- src/components/korea-map-leaflet.tsx
+++ src/components/korea-map-leaflet.tsx
@@ -232,7 +232,7 @@ export default function LeafletMapComponent({
     <MapContainer
       center={[36.5, 127.8]}
       zoom={7}
-      style={{ width: "100%", height: "320px" }}
+      style={{ width: "100%", height: "450px" }}
       scrollWheelZoom={false}
       zoomControl={true}
     >
